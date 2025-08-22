a1cbf5ace72ed9fcbe9667bbada18a83c4f81c2e
WayuAji30 <wahyuma123@gmail.com>
2025-08-22 14:26:36 +0700

    add: ai art

--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -64,7 +64,7 @@ export function NavigationMenuNav() {
                 <NavigationMenuLink asChild>
                   <a
                     className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-6 no-underline outline-hidden select-none focus:shadow-md"
-                    href="/"
+                    href=""
                   >
                     <div className="mt-4 mb-2 text-lg font-medium">
                       shadcn/ui
